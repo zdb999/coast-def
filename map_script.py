705720b2c96bc0815946828d3a85b6fba89c769d
Zach <zdb999@gmail.com>
2018-12-17 01:21:23 -0500

    Fixed the islands issue!

--- map_script.py
+++ map_script.py
@@ -1,37 +1,18 @@
 import cv2
 import numpy as np
-from osgeo import gdal, gdalnumeric, ogr, osr
-from cv2 import VideoWriter
-from PIL import ImageFont, ImageDraw, Image
 from coastdef.animate import animate_flood
 import coastdef.utils as utils
 from coastdef.geo import make_extent_layer
-gdal.UseExceptions()
-
-
-def world2Pixel(geoMatrix, x, y):
-  """
-  Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate
-  the pixel location of a geospatial coordinate
-  """
-  ulX = geoMatrix[0]
-  ulY = geoMatrix[3]
-  xDist = geoMatrix[1]
-  yDist = geoMatrix[5]
-  rtnX = geoMatrix[2]
-  rtnY = geoMatrix[4]
-  pixel = int((x - ulX) / xDist)
-  line = int((ulY - y) / xDist)
-  return (pixel, line)
-
 
 e1 = cv2.getTickCount()
 
-
-
 make_extent_layer('test2.tiff', [(5000, 4000)], 14)
 
-animate_flood('test2.tiff', [(5000, 4000)], 7, 12, 1, 2)
+animate_flood('test2.tiff', [(5000, 4000)], 0, 80, 1, 8)
+
+# make_extent_layer('test.tiff', [(5, 5)], 37)
+
+# animate_flood('test.tiff', [(5, 5), (650,1050), (1050,650)], 3, 83, 1, 10)
 
 e2 = cv2.getTickCount()
 time = (e2 - e1)/ cv2.getTickFrequency()
